8259b1f1f2294011195120f4b3fc650ccbd965a3
Peter Shen <petershen0307@hotmail.com>
2016-03-02 23:16:03 +0800

    [what] rename function list_all_permutation() to list_all_factorial_permutation(), add new function list_all_exponential_permutation()
    [why] give the more specific function name
    [ToDo] implement function list_all_exponential_permutation()

--- Test_calculate24_multi_digit.py
+++ Test_calculate24_multi_digit.py
@@ -3,15 +3,18 @@ from calculate24_multi_digit import *
 
 
 class TestFind24MultiDigit(unittest.TestCase):
-    def test_exhaustion_factorial(self):
-        self.assertEqual(list_all_permutation(['+']), [['+']])
-        self.assertEqual(list_all_permutation([1]), [[1]])
-        self.assertEqual(list_all_permutation([1, 2]), [[1, 2], [2, 1]])
-        self.assertEqual(list_all_permutation([1, 2, 3]),
+    def test_list_all_exponential_permutation(self):
+        self.assertEqual(list_all_exponential_permutation(1, ['+', '-', '*', '/']), [['+', '-', '*', '/']])
+
+    def test_list_all_factorial_permutation(self):
+        self.assertEqual(list_all_factorial_permutation(['+']), [['+']])
+        self.assertEqual(list_all_factorial_permutation([1]), [[1]])
+        self.assertEqual(list_all_factorial_permutation([1, 2]), [[1, 2], [2, 1]])
+        self.assertEqual(list_all_factorial_permutation([1, 2, 3]),
                          [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]])
-        self.assertEqual(list_all_permutation([1, 2, '3']),
+        self.assertEqual(list_all_factorial_permutation([1, 2, '3']),
                          [[1, 2, '3'], [1, '3', 2], [2, 1, '3'], [2, '3', 1], ['3', 1, 2], ['3', 2, 1]])
-        self.assertEqual(list_all_permutation([11, 22, '3', '+']),
+        self.assertEqual(list_all_factorial_permutation([11, 22, '3', '+']),
                          [[11, 22, '3', '+'], [11, 22, '+', '3'], [11, '3', 22, '+'],
                           [11, '3', '+', 22], [11, '+', 22, '3'], [11, '+', '3', 22],
                           [22, 11, '3', '+'], [22, 11, '+', '3'], [22, '3', 11, '+'],
--- calculate24_multi_digit.py
+++ calculate24_multi_digit.py
@@ -1,7 +1,15 @@
 from fractions import Fraction
 
 
-def list_all_permutation(components):
+def list_all_exponential_permutation(n, inputs):
+    # 輸入: 由int + str組成的list
+    # 輸出: list of list, 指數排列組合
+    if 1 == n:
+        return [inputs]
+    return None
+
+
+def list_all_factorial_permutation(components):
     # 輸入: int + str(+-*/) 的 list, 個數都是0~多個
     # 輸出: int與str的所有排列組合
     if 1 == len(components):
@@ -10,7 +18,7 @@ def list_all_permutation(components):
     for element in components:
         temp_components = list(components)
         temp_components.remove(element)
-        results = list_all_permutation(temp_components)
+        results = list_all_factorial_permutation(temp_components)
         for sub_result in results:
             if len(sub_result) >= len(components):
                 continue
@@ -21,4 +29,4 @@ def list_all_permutation(components):
 
 
 if '__main__' == __name__:
-    print(list_all_permutation([11, 22, '3', '+']))
+    print(list_all_factorial_permutation([11, 22, '3', '+']))
